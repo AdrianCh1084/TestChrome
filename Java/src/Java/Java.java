e926976eca069e9f290d7a30f64f6ab8a7ff46db
Adrian-PC\Adrian <a.chmilowski@interia.pl>
2017-12-28 19:04:11 +0100

    Added public void

--- Java/src/Java/Java.java
+++ Java/src/Java/Java.java
@@ -1,11 +1,15 @@
 package Java;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Java {
 	
+	public void main(String[] arg) {
 	
-	WebDriver driver = new ChromeDriver ();
+	WebDriver driver = new ChromeDriver();
 
 	driver.get("www.topmaison.pl");
 	
-	
+	}
 }
